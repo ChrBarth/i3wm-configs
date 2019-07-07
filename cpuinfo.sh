647fb6bfea6fb230347e8ff6ffd42ce2de8d3a98
ChrBarth <ChrBarth@users.noreply.github.com>
2019-07-07 15:14:46 +0200

    added some comments to cpuinfo/cputemp scripts and updated README

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# outputs current current frequency, governor and temperature for each cpu core
+
 CPUDIR="/sys/bus/cpu/devices/"
+
 for cpu in `ls $CPUDIR`
 do
     freqpath="${CPUDIR}${cpu}/cpufreq/"
     echo -n $cpu
     echo -n ": "
+    # using awk for math because why not :)
     cat "${freqpath}scaling_cur_freq" | awk '/.*/ {printf("%.2f GHz", $1/1000000)}'
     echo -n " ( "
     echo `cat "${freqpath}scaling_governor"` ")"
--- cputemp.py
+++ cputemp.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import subprocess
-#import re
 
 # get the cpu-temerature(s) from lm-sensors
+# for intel i3 on MSI B75A-G43
 
 # sensors -u outputs the temperature-data in raw
 CMD = ['sensors', '-u']
@@ -17,15 +17,11 @@ temp2 = 0
 temp3 = 0
 
 for l in enumerate(result):
-    #print("{} --> {}".format(l[0], l[1]))
     if 'Package id 0' in l[1]:
         temp1 = float(result[l[0]+1].split(": ")[1])
-        #print("Temp1: {}".format(int(temp1)))
     if 'Core 0' in l[1]:
         temp2 = float(result[l[0]+1].split(": ")[1])
-        #print("Temp2: {}".format(int(temp2)))
     if 'Core 1' in l[1]:
         temp3 = float(result[l[0]+1].split(": ")[1])
-        #print("Temp3: {}".format(int(temp3)))
 
 print("{}°C / {}°C / {}°C".format(int(temp1), int(temp2), int(temp3)))
