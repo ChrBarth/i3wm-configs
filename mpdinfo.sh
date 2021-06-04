4f3feb90500366da81379c16ed32402b804ece56
ChrBarth <ChrBarth@users.noreply.github.com>
2021-06-04 16:31:25 +0200

    nicer mpd info

--- mpdinfo.sh
+++ mpdinfo.sh
@@ -4,4 +4,15 @@
 # to prevent "Error: Could not parse JSON..." in i3bar
 # (conky does not have an option to escape or filter these
 # characters...
-mpc status -f "%artist%\t%title%" | sed -e 's/[\"]//g' -e "s/[\'\`\']//g" -n -e 's/\t/ - /g p'
+PLAYING="⏹️"
+SONGINFO=$(mpc status -f "%artist%\t%title%" | sed -e 's/[\"]//g' -e "s/[\'\`\']//g" -n -e 's/\t/ - /g p')
+STATUS=$(mpc status | sed -n -E 's/\[(playing|paused)\].*/\1/p')
+if [ "$STATUS" = "paused" ];
+then
+    PLAYING="⏸️"
+elif [ $STATUS = "playing" ];
+then
+    PLAYING="▶️"
+fi
+
+echo $PLAYING $SONGINFO
