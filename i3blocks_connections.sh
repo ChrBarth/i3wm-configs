0e17364d261d97817df968253b17d126abe9592d
ChrBarth <ChrBarth@users.noreply.github.com>
2022-04-10 11:16:27 +0200

    st-> sakura, updated some shortcuts, minor changes

--- i3blocks_connections.sh
+++ i3blocks_connections.sh
@@ -4,11 +4,11 @@
 #jack_lsp -c | awk 'BEGIN { FS="\n"; con=0 } /^[a-zA-Z]/ { line=$1 } /   system:capture_[12]/ { if(match(line, "system:playback")) {print line, $1; con=con+1}} END { print "Connections:", con }'
 CON=$(jack_lsp -c | awk 'BEGIN { FS="\n"; con=0 } /^[a-zA-Z]/ { line=$1 } /   system:capture_[12]/ { if(match(line, "system:playback")) {con=con+1}} END { print con }')
 if [[ $CON -eq 2 ]]; then
-    echo "(connected)"
-    echo
+    #echo "(connected)"
+    echo 😸
     echo "#00FF00"
 else
-    echo "(not connected)"
-    echo
+    #echo "(not connected)"
+    echo 🙀
     echo "#AAAAAA"
 fi
--- i3blocks_jack.sh
+++ i3blocks_jack.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # is jack up and running?
 JACK_STATUS=$(jack_control status | tail -n 1)
-echo "JACK: $JACK_STATUS"
-echo "$JACK_STATUS"
+#echo "$JACK_STATUS"
 if [ "$JACK_STATUS" = "stopped" ]; then
+    echo "❌"
     echo "#FF0000"
 else
+    echo "✅"
     echo "#00FF00"
 fi
